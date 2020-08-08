64f8016e07bf14151e777995be8d16712d8a4640
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-08-08 13:04:34 +0500

    Redefine UnitTypeBuilder

--- src/main/java/agenta/UnitTypeBuilder.java
+++ src/main/java/agenta/UnitTypeBuilder.java
@@ -1,73 +1,58 @@
 package agenta;
 
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-
 public class UnitTypeBuilder
 {
-    public static final int MIN_PRIORITY = 1;
-    public static final int MAX_PRIORITY = 5;
-
-    private String name = "";
-    private int hitPoints = 0;
-    private int baseAttack = 0;
-    private int randAttack = 0;
-    private int attackSpeed = 0;
-    private float range = 0f;
-    private int visibility = 0;
-    private int speed = 0;
-    private int[] healthLimit = new int[MAX_PRIORITY];
+    private final UnitTypeImpl type = new UnitTypeImpl();
 
     public UnitTypeBuilder setAttackSpeed(int value)
     {
-        attackSpeed = value;
+        type.setAttackSpeed(value);
         return this;
     }
 
     public UnitTypeBuilder setBaseAttack(int value)
     {
-        baseAttack = value;
+        type.setBaseAttack(value);
         return this;
     }
 
     public UnitTypeBuilder setHitPoints(int value)
     {
-        hitPoints = value;
-        for (int i = 0; i < healthLimit.length; i++)
-        {
-            healthLimit[i] = (4 - i) * hitPoints / 5;
-        }
+        type.setHitPoints(value);
         return this;
     }
 
     public UnitTypeBuilder setName(String name)
     {
-        this.name = name;
+        type.setName(name);
         return this;
     }
 
     public UnitTypeBuilder setRandAttack(int value)
     {
-        randAttack = value;
+        type.setRandAttack(value);
         return this;
     }
 
     public UnitTypeBuilder setRange(float value)
     {
-        range = value;
+        type.setRange(value);
         return this;
     }
 
     public UnitTypeBuilder setSpeed(int value)
     {
-        speed = value;
+        type.setSpeed(value);
         return this;
     }
 
     public UnitTypeBuilder setVisibility(int value)
     {
-        visibility = value;
+        type.setVisibility(value);
         return this;
+    }
+
+    public UnitTypeImpl build() {
+        return new UnitTypeImpl(type);
     }
 }
