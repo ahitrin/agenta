655286644d5fa1f61b65dd28bcb927a09027c1f9
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-08-08 16:02:22 +0500

    Run experiment for 100 times

--- src/main/java/agenta/experiment/Baseline.java
+++ src/main/java/agenta/experiment/Baseline.java
@@ -1,6 +1,8 @@
 package agenta.experiment;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import agenta.BaseCommander;
 import agenta.Engine;
@@ -15,11 +17,81 @@ import agenta.UnitTypeBuilder;
  */
 public class Baseline
 {
+    private static final class RunResult
+    {
+        public final long winner;
+        public final long steps;
+
+        private RunResult(long winner, long steps)
+        {
+            this.winner = winner;
+            this.steps = steps;
+        }
+    }
+
     public static void main(String[] args)
+    {
+        final List<UnitType> unitTypes = buildUnitTypes();
+        InputParameters params = buildParams(unitTypes);
+
+        List<RunResult> results = IntStream.range(0, 100)
+                .mapToObj(i -> singleRun(unitTypes, params))
+                .collect(Collectors.toList());
+        long wins0 = 0, wins1 = 0, p0 = 0, p1 = 0, draws = 0, steps0 = 0, steps1 = 0;
+        for (RunResult r : results) {
+            if (r.winner == 0) {
+                wins0++;
+                steps0 += r.steps;
+            }
+            else if (r.winner == 1) {
+                wins1++;
+                steps1 += r.steps;
+            }
+            else {
+                draws++;
+            }
+        }
+        if (wins0 > 0) {
+            steps0 /= wins0;
+        }
+        if (wins1 > 0) {
+            steps1 /= wins1;
+        }
+        if (wins0 + wins1 > 0) {
+            p0 = 100 * wins0 / (wins0 + wins1);
+            p1 = 100 * wins1 / (wins0 + wins1);
+        }
+        System.out.printf("Player 0 won %d times (%d%%), in %d steps average%n", wins0, p0, steps0);
+        System.out.printf("Player 1 won %d times (%d%%), in %d steps average%n", wins1, p1, steps1);
+        System.out.printf("Total %d draws (battle runs for too long)%n", draws);
+        System.out.printf("Total runs: %d%n", wins0 + wins1 + draws);
+    }
+
+    private static RunResult singleRun(List<UnitType> unitTypes, InputParameters params)
+    {
+        Engine e = new Engine(params, SingleRandom.get());
+        e.init(unitTypes);
+        int steps = 0;
+        while (e.getWinner() == -1 && steps < 1000)
+        {
+            e.step();
+            steps++;
+        }
+        System.out.printf("Player %d has won after %d steps%n", e.getWinner(), steps);
+        return new RunResult(e.getWinner(), steps);
+    }
+
+    private static InputParameters buildParams(List<UnitType> unitTypes)
     {
         InputParameters params = new InputParameters();
         params.addCommander(new BaseCommander());
         params.addCommander(new BaseCommander());
+        unitTypes.forEach(ut -> params.addUnit(ut.getName().toLowerCase(), 30, 30));
+        return params;
+    }
+
+    private static List<UnitType> buildUnitTypes()
+    {
         UnitType warrior = new UnitTypeBuilder()
                 .setName("Warrior")
                 .setAttackSpeed(3)
@@ -30,14 +102,7 @@ public class Baseline
                 .setVisibility(10)
                 .setRange(1.45f)
                 .build();
-        params.addUnit(warrior.getName().toLowerCase(), 30, 30);
-        Engine e = new Engine(params, SingleRandom.get());
-        e.init(List.of(warrior));
-        int steps = 0;
-        while (e.getWinner() == -1 && steps < 1000) {
-            e.step();
-            steps++;
-        }
-        System.out.printf("Player %d has won after %d steps%n", e.getWinner(), steps);
+        final List<UnitType> unitTypes = List.of(warrior);
+        return unitTypes;
     }
 }
