2dc9266726cdd8f7a4ae7a90ede833df36437988
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-08-23 13:25:45 +0500

    InputParameters: do not store useless "commanders"

--- src/main/java/agenta/InputParameters.java
+++ src/main/java/agenta/InputParameters.java
@@ -1,43 +1,21 @@
 package agenta;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public final class InputParameters
 {
-    private final Commander[] commanders = new Commander[2];
-    private int commanderCounter = 0;
-    private final java.util.Map<String, Integer> player0Units = new HashMap<>();
-    private final java.util.Map<String, Integer> player1Units = new HashMap<>();
-
-    public InputParameters()
-    {
-        commanders[0] = null;
-        commanders[1] = null;
-    }
+    private final Map<String, Integer> player0Units = new HashMap<>();
+    private final Map<String, Integer> player1Units = new HashMap<>();
 
     public void addCommander(Commander commander)
     {
-        if (commanderCounter >= 2)
-        {
-            return;
-        }
-        commanders[commanderCounter] = commander;
-        commanderCounter++;
     }
 
     public void addUnit(String name, int player0, int player1)
     {
         player0Units.put(name, player0);
         player1Units.put(name, player1);
-    }
-
-    public Commander getCommander(int index)
-    {
-        if ((index >= 0) && (index < commanderCounter))
-        {
-            return commanders[index];
-        }
-        return null;
     }
 
     public int getUnit(int player, String name)
