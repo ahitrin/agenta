70fb02a2dc7409f59d1b534d5864ebe81280e9d5
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-11-28 20:42:04 +0500

    Some fixes in lein project definition

--- project.clj
+++ project.clj
@@ -3,5 +3,12 @@
   :url "https://github.com/ahitrin/agenta"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.github.javafaker/javafaker "1.0.2"]
+                 [org.apache.logging.log4j/log4j-api "2.13.3"]
+                 [org.apache.logging.log4j/log4j-core "2.13.3"]
+                 [org.junit.jupiter/junit-jupiter-api "5.6.2"]
+                 [org.junit.jupiter/junit-jupiter-engine "5.6.2"]]
+  :java-source-paths ["src/main/java"
+                      "src/test/java"]
   :repl-options {:init-ns agenta.core})
--- test/agenta/core_test.clj
+++ test/agenta/core_test.clj
@@ -3,5 +3,5 @@
             [agenta.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Simple dummy test"
+    (is (= 1 1))))
