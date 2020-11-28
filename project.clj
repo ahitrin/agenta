9a8f15e04016e72cff61c2fd3dfd6bcf3c192d3f
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-11-28 20:25:22 +0500

    Updaet project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject agenta "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple agent-oriented AI 'game'"
+  :url "https://github.com/ahitrin/agenta"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
