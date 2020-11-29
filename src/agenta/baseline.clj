c13a149ee34f26780e7c37f36a61a73b6acd8fe8
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-11-29 20:00:41 +0500

    Get rid of Baseline

---
+++ src/agenta/baseline.clj
@@ -0,0 +1,22 @@
+(ns agenta.baseline
+  (:import (agenta UnitTypeBuilder)
+           (agenta.experiment Experiment)))
+
+; Baseline experiment: each of two players with same forces should have ~50% chance to win
+
+(def unit-types
+  [(-> (UnitTypeBuilder.)
+       (.setName "Warrior")
+       (.setAttackSpeed 3)
+       (.setBaseAttack 1)
+       (.setRandAttack 4)
+       (.setHitPoints 10)
+       (.setSpeed 3)
+       (.setVisibility 10)
+       (.setRange 1.45)
+       (.build))])
+
+(defn -main [& args]
+  (let [player-units {"warrior" 10}
+        results (Experiment/runExperiment 100 5000 unit-types player-units player-units)]
+    (Experiment/calculateStatistics results)))
