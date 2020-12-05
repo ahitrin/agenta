179a2c48fc012857273769a65e60ffc7455ba28f
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-12-05 15:02:44 +0500

    Get rid of RunResult class

--- src/agenta/experiment.clj
+++ src/agenta/experiment.clj
@@ -1,6 +1,5 @@
 (ns agenta.experiment
-  (:import (agenta Engine SingleRandom)
-           (agenta.experiment RunResult)))
+  (:import (agenta Engine SingleRandom)))
 
 (defn single-run
   [max-ticks unit-types player0-units player1-units]
@@ -12,7 +11,7 @@
     (println (String/format "Player %d has won after %d ticks"
                             (object-array [(.getWinner e)
                                            (.getTicks e)])))
-    (RunResult. (.getWinner e) (.getTicks e))))
+    {:winner (.getWinner e) :steps (.getTicks e)}))
 
 (defn run-experiment
   [total-experiments max-ticks unit-types player0-units player1-units]
@@ -20,12 +19,12 @@
 
 (defn calc-statistics
   [results]
-  (let [wins-per-player (group-by #(.-winner %) results)
+  (let [wins-per-player (group-by :winner results)
         wins0 (count (wins-per-player 0))
         wins1 (count (wins-per-player 1))
         draws (count (wins-per-player -1))
-        steps0 (if (zero? wins0) 0 (double (/ (reduce + (map #(.-steps %) (wins-per-player 0))) wins0)))
-        steps1 (if (zero? wins1) 0 (double (/ (reduce + (map #(.-steps %) (wins-per-player 1))) wins1)))
+        steps0 (if (zero? wins0) 0 (double (/ (reduce + (map :steps (wins-per-player 0))) wins0)))
+        steps1 (if (zero? wins1) 0 (double (/ (reduce + (map :steps (wins-per-player 1))) wins1)))
         p0 (if (zero? (+ wins0 wins1)) 0 (double (/ (* 100 wins0) (+ wins0 wins1))))
         p1 (if (zero? (+ wins0 wins1)) 0 (- 100 p0))]
     (printf "Player 0 won %d times (%f%%), in %f steps average%n" wins0 p0 steps0)
