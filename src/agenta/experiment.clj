1b9eb1aa75616544b62fff7ad900b832011bafbe
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-12-05 15:00:25 +0500

    Fix: use the right property

--- src/agenta/experiment.clj
+++ src/agenta/experiment.clj
@@ -24,8 +24,8 @@
         wins0 (count (wins-per-player 0))
         wins1 (count (wins-per-player 1))
         draws (count (wins-per-player -1))
-        steps0 (if (zero? wins0) 0 (double (/ (reduce + (map #(.-winner %) (wins-per-player 0))) wins0)))
-        steps1 (if (zero? wins1) 0 (double (/ (reduce + (map #(.-winner %) (wins-per-player 1))) wins1)))
+        steps0 (if (zero? wins0) 0 (double (/ (reduce + (map #(.-steps %) (wins-per-player 0))) wins0)))
+        steps1 (if (zero? wins1) 0 (double (/ (reduce + (map #(.-steps %) (wins-per-player 1))) wins1)))
         p0 (if (zero? (+ wins0 wins1)) 0 (double (/ (* 100 wins0) (+ wins0 wins1))))
         p1 (if (zero? (+ wins0 wins1)) 0 (- 100 p0))]
     (printf "Player 0 won %d times (%f%%), in %f steps average%n" wins0 p0 steps0)
