682fb7129c9bcb86cfa24229c7f389afeffaccbf
Andrey Hitrin <andrey.hitrin@gmail.com>
2021-02-06 16:57:43 +0500

    Simplify init atom

--- src/agenta/ui.clj
+++ src/agenta/ui.clj
@@ -16,21 +16,20 @@
         val))))
 
 (defn wrap-viewer [^JFrame f ^ImagePanel p]
-  (let [state (atom {:need-init true})
-        image-cache (simple-cache)]
+  (let [need-init (atom true) image-cache (simple-cache)]
     (fn [m]
       (let [size-x (gm/size-x m)
             size-y (gm/size-y m)
             pix-x (* 25 size-x)
             pix-y (* 25 size-y)]
-        (when (:need-init @state)
+        (when @need-init
           (.setSize p pix-x pix-y)
           (.setVisible p true)
           (.setSize f pix-x pix-y)
           (.setVisible f true)
           (.add (.getContentPane f) ^Component p)
           (.setDefaultCloseOperation f JFrame/EXIT_ON_CLOSE)
-          (swap! state assoc :need-init false))
+          (swap! need-init not))
         (let [image (cast BufferedImage (.createImage p pix-x pix-y))
               currentGraph (.createGraphics image)]
           (doseq [i (range size-x)
