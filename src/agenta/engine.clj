d108679b094a3ea9a0be6e0282be0cbeef915acc
Andrey Hitrin <andrey.hitrin@gmail.com>
2021-10-09 23:01:50 +0500

    Add important comment

--- src/agenta/engine.clj
+++ src/agenta/engine.clj
@@ -70,6 +70,7 @@
 
 (defn- perform-attack! [m actor action [x y]]
   (let [target-id (int (.get action "target"))
+        ; TODO replace this (very ineffective) calculation with some kind of map :id -> unit
         target (:old (first (filter #(= target-id (:id %)) (vals (:objs m)))))]
     (if (some? target)
       (let [damage (.doAttack (:old actor))
