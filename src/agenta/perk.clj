5cfb2ddca1f13d218e1c5fc5252659b65b5ef5fc
Andrey Khitrin <akhitrin@gridgain.com>
2022-11-12 10:35:25 +0400

    perk.clj: extract as-java-fn wrapper

--- src/agenta/perk.clj
+++ src/agenta/perk.clj
@@ -2,12 +2,13 @@
   (:require [agenta.random :as rnd])
   (:import (java.util.function Function)))
 
+(defn as-java-fn [fun]
+  (proxy [Function] [] (apply [arg] (apply fun [arg]))))
+
 (defn select-random
   "Given several units select one of them absolutely randomly"
   []
-  (proxy [Function] []
-    (apply [units]
-      (.get units (rnd/i! (count units))))))
+  (as-java-fn (fn [units] (.get units (rnd/i! (count units))))))
 
 (defn select-random-memoized
   "Remember previously selected unit and try to select it when possible; else do it randomly"
@@ -17,14 +18,11 @@
                  (cond
                    (and (some? cur) (pos? (.-currentHitPoints cur)) (.contains new-units cur)) cur
                    (false? (.isEmpty new-units)) (.get new-units (rnd/i! (count new-units)))
-                   :else nil))]
-    (proxy [Function] []
-      (apply [units]
-        (swap! target choose units)))))
+                   :else nil))
+        make-select (fn [units] (swap! target choose units))]
+    (as-java-fn make-select)))
 
 (defn select-weakest
   "Given several units select the one who has fewer hit points"
   []
-  (proxy [Function] []
-    (apply [units]
-      (first (sort-by #(.-currentHitPoints %) units)))))
+  (as-java-fn (fn [units] (first (sort-by #(.-currentHitPoints %) units)))))
