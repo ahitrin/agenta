1e5e5e2deaaab3daf94b366ae028f3e29f13fe78
Andrey Hitrin <andrey.hitrin@gmail.com>
2022-11-20 20:45:03 +0500

    Remove unused wrapper

--- src/agenta/perk.clj
+++ src/agenta/perk.clj
@@ -1,9 +1,5 @@
 (ns agenta.perk
-  (:require [agenta.random :as rnd])
-  (:import (java.util.function Function)))
-
-(defn as-java-fn [fun]
-  (proxy [Function] [] (apply [arg] (apply fun [arg]))))
+  (:require [agenta.random :as rnd]))
 
 (defn select-random
   "Given several units select one of them absolutely randomly"
