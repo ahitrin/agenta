c27bf0cb79ab2962edf4d79ef186cb485142090f
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-03-11 22:29:09 +0500

    Revert "Pass new units to perks"
    This reverts commit 2d4e239db72ccc3edd63f026e0b082178b8fc376.

--- src/agenta/perk.clj
+++ src/agenta/perk.clj
@@ -1,6 +1,5 @@
 (ns agenta.perk
-  (:require [agenta.random :as rnd])
-  (:import (agenta Unit)))
+  (:require [agenta.random :as rnd]))
 
 (defn select-random
   "Given several units select one of them absolutely randomly"
@@ -11,17 +10,14 @@
   "Remember previously selected unit and try to select it when possible; else do it randomly"
   [units]
   (let [target (atom nil)
-        choose (fn [^Unit cur new-units]
+        choose (fn [cur new-units]
                  (cond
-                   (and (some? cur)
-                        (pos? (.-currentHitPoints cur))
-                        (.contains (map :id new-units) (.getId cur)))
-                   cur
-                   (seq new-units) (:old (.get new-units (rnd/i! (count new-units))))
+                   (and (some? cur) (pos? (.-currentHitPoints cur)) (.contains new-units cur)) cur
+                   (false? (.isEmpty new-units)) (.get new-units (rnd/i! (count new-units)))
                    :else nil))]
     (swap! target choose units)))
 
 (defn select-weakest
   "Given several units select the one who has fewer hit points"
   [units]
-  (first (sort-by #(.-currentHitPoints (:old %)) units)))
+  (first (sort-by #(.-currentHitPoints %) units)))
