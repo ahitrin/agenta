d0246d06772b33336fea5089fdd91e9d155ab76f
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-03-12 22:42:20 +0500

    Build :id->xy index on game map recreation

--- src/agenta/game_map.clj
+++ src/agenta/game_map.clj
@@ -1,10 +1,12 @@
 (ns agenta.game-map
   (:require [agenta.random :as rnd]))
 
-(defrecord GameMap [size-x size-y cells objs])
+(defrecord GameMap [size-x size-y cells objs objs-idx])
 
 (defn new-map [^GameMap m objs-fn]
-  (GameMap. (:size-x m) (:size-y m) (:cells m) (-> (:objs m) objs-fn)))
+  (let [new-objs (-> (:objs m) objs-fn)
+        new-idx (reduce-kv (fn [m k v] (assoc m (:id v) k)) {} new-objs)]
+    (GameMap. (:size-x m) (:size-y m) (:cells m) new-objs new-idx)))
 
 (defn plane
   "Fill given rectangle with grass solely"
@@ -36,15 +38,16 @@
   (let [size-x (:size-x map-spec)
         size-y (:size-y map-spec)
         type (resolve (:type map-spec))
-        m (GameMap. size-x size-y (apply type [size-x size-y]) {})
+        m (GameMap. size-x size-y (apply type [size-x size-y]) {} {})
         xys (filter #(can-place? m %)
                     (distinct (repeatedly #(rnd/xy! size-x size-y))))
         objs (zipmap xys units)]
     (new-map m #(into % objs))))
 
 (defn obj-by-id [m oid]
-  ; TODO replace this (very ineffective) calculation with some kind of map :id -> obj
-  (first (filter #(= oid (:id (val %))) (:objs m))))
+  (let [xy (.get (:objs-idx m) oid)
+        obj (.get (:objs m) xy)]
+    (first {xy obj})))
 
 (defn objects-in-radius [m x y r]
   (let [limit (int r)]
