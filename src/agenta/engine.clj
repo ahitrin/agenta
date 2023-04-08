f970f3fa8385777e2bb0945a1a73fec3331e213e
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-04-08 15:48:07 +0500

    Use perk from settings

--- src/agenta/engine.clj
+++ src/agenta/engine.clj
@@ -104,7 +104,7 @@
           {:type :move :dx dx :dy dy})
         ; random move
         :else
-        (pk/move-randomly actor visible-objects)))))
+        (apply (:move-perk actor) [actor visible-objects])))))
 
 (defn unit-action! [[[x y] u] m]
   (let [max-hp (:max-health u)
