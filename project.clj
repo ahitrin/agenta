6521683dfa4e764fb76a6e02aaa2b45b0adf72f8
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-09-21 20:35:03 +0500

    Update clojure dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ahitrin/agenta"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.1.0"]
                  [com.github.javafaker/javafaker "1.0.2"]
                  [ch.qos.logback/logback-classic "1.1.2"]
