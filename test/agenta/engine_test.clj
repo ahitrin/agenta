0a29ef442c586fb99bff4f76ba08d6b2c7789424
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-10-07 17:40:13 +0500

    engine_test: remove NPE (tests do still fail though)

--- test/agenta/engine_test.clj
+++ test/agenta/engine_test.clj
@@ -1,18 +1,26 @@
 (ns agenta.engine-test
   (:require [clojure.test :refer :all]
             [agenta.engine :refer :all]
-            [agenta.counter :as ctr]))
+            [agenta.counter :refer [make]]))
 
 (deftest tick-health-test
   (testing "unit healt counter decreased after tick"
-    (let [objs {[0 0] {:name "Fooer" :health-counter (ctr/make 4 5)}
-                [1 1] {:name "Barry" :health-counter (ctr/make 2 5)}}
-          target {[0 0] {:name "Fooer" :health-counter (ctr/make 3 5)}
-                  [1 1] {:name "Barry" :health-counter (ctr/make 1 5)}}]
+    (let [objs {[0 0] {:name "Fooer"
+                       :health-counter (make 4 5)
+                       :speed-counter (make 4 5)
+                       :attack-counter (make 4 5)
+                       :think-counter (make 4 5)
+                       :health 10
+                       :max-health 10}}
+          target {[0 0] {:name "Fooer" :health-counter (make 3 5)}}]
       (is (= target (tick-health objs)))))
   (testing "counter that reaches zero should be reset after tick"
-    (let [objs {[0 0] {:name "Fooer" :health-counter (ctr/make 4 5)}
-                [1 1] {:name "Barry" :health-counter (ctr/make 1 5)}}
-          target {[0 0] {:name "Fooer" :health-counter (ctr/make 3 5)}
-                  [1 1] {:name "Barry" :health-counter (ctr/make 5 5)}}]
+    (let [objs {[1 1] {:name "Barry"
+                       :health-counter (make 1 5)
+                       :speed-counter (make 4 5)
+                       :attack-counter (make 4 5)
+                       :think-counter (make 4 5)
+                       :health 10
+                       :max-health 10}}
+          target {[1 1] {:name "Barry" :health-counter (make 5 5)}}]
       (is (= target (tick-health objs))))))
