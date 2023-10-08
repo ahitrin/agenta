c672d8fb2211055de02af7a95c44a77243fd35d9
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-10-08 22:16:32 +0500

    Extract dev profile in lein

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/ahitrin/agenta"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[dev.weavejester/lein-cljfmt "0.11.2"]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.1.0"]
                  [cljfx "1.7.24"]
                  [com.github.javafaker/javafaker "1.0.2"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
-                 [com.clojure-goes-fast/clj-async-profiler "1.0.5"]]
+                 [ch.qos.logback/logback-classic "1.1.2"]]
+  :profiles {:dev {:plugins [[dev.weavejester/lein-cljfmt "0.11.2"]]
+                   :dependencies [[com.clojure-goes-fast/clj-async-profiler "1.0.5"]]}}
   :java-source-paths ["src/main/java"
                       "src/test/java"]
   :resource-paths ["resources"]
