74cf19461ab95bbbced8566746ba9269e3af9814
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-10-14 17:53:57 +0500

    fxui: simple grid pane

--- src/agenta/fxui.clj
+++ src/agenta/fxui.clj
@@ -6,13 +6,21 @@
 
 (defmulti event-handler :event/type)
 
+(def grid-pane
+  {:fx/type :grid-pane
+  :children (for [i (range 10)
+                  j (range 10)]
+              {:fx/type :label
+               :grid-pane/row i
+               :grid-pane/column j
+               :text (str "[" i "/" j "]")})})
+
 (defn root-view [{{:keys []} :state}]
   {:fx/type :stage
    :showing true
    :scene {:fx/type :scene
            :root {:fx/type :h-box
-                  :children [{:fx/type :label
-                              :text "agenta"}]}}})
+                  :children [grid-pane]}}})
 
 (def renderer
   (fx/create-renderer
