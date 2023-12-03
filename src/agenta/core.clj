a298a2e341c286ee1d53521b61f4dd44198919fc
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-12-03 18:19:17 +0500

    core: use new fns instead of run-game!

--- src/agenta/core.clj
+++ src/agenta/core.clj
@@ -6,6 +6,7 @@
 (defn -main [& args]
   (rnd/init!)
   (let [i (if (seq? args) (first args) "demo")
-        s (clojure.edn/read-string (slurp (format "setting/%s.edn" i)))]
+        s (clojure.edn/read-string (slurp (format "setting/%s.edn" i)))
+        g (eng/init-game s)]
     (ui/show)
-    (eng/run-game! s ui/update-state!)))
+    (eng/run-loop! ui/update-state! (:map g) -1 (:tick g) (:end-tick g) (fn [] false))))
