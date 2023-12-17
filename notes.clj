1d2c680240fa4610ddc2ba1e2f27517bc992bbd0
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-12-17 19:06:45 +0500

    Add watch paths to clerk

--- notes.clj
+++ notes.clj
@@ -1,6 +1,7 @@
 (require '[nextjournal.clerk :as clerk])
 
 ;; start Clerk webserver
-(clerk/serve! {:browse true})
+(clerk/serve! {:browse true
+               :watch-paths ["notebooks"]})
 
 (clerk/show! "notebooks/01.clj")
