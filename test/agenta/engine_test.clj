d77a6900efb48159f1f0894b76acea6ce2c67d57
Andrey Hitrin <andrey.hitrin@gmail.com>
2024-11-04 12:02:51 +0500

    Remove old broken test

--- test/agenta/engine_test.clj
+++ test/agenta/engine_test.clj
@@ -5,40 +5,6 @@
             [agenta.game-map    :refer [make-map]]
             [agenta.random      :refer [init!]]))
 
-(deftest tick-health-test
-  (testing "unit healt counter decreased after tick"
-    (let [objs {[0 0] {:name "Fooer"
-                       :health-counter (make 4 5)
-                       :speed-counter (make 4 5)
-                       :attack-counter (make 4 5)
-                       :think-counter (make 4 5)
-                       :health 10
-                       :max-health 10}}
-          target {[0 0] {:name "Fooer"
-                         :health-counter (make 3 5)
-                         :speed-counter (make 3 5)
-                         :attack-counter (make 3 5)
-                         :think-counter (make 3 5)
-                         :health 10
-                         :max-health 10}}]
-      (is (= target (tick-health objs)))))
-  (testing "counter that reaches zero should be reset after tick"
-    (let [objs {[1 1] {:name "Barry"
-                       :health-counter (make 1 5)
-                       :speed-counter (make 1 5)
-                       :attack-counter (make 1 5)
-                       :think-counter (make 1 5)
-                       :health 9
-                       :max-health 10}}
-          target {[1 1] {:name "Barry"
-                         :health-counter (make 5 5)
-                         :speed-counter (make 0 5)
-                         :attack-counter (make 0 5)
-                         :think-counter (make 0 5)
-                         :health 10
-                         :max-health 10}}]
-      (is (= target (tick-health objs))))))
-
 (deftest produce-messages-test
   (init! 777)
   (let [setting     (clojure.edn/read-string (slurp "setting/smoke.edn"))
