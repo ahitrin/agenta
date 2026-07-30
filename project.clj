f3f0a52927c1f83282a2471e64660b7e291c3f47
Andrey Hitrin <andrey.hitrin@gmail.com>
2026-07-30 22:55:53 +0500

    Update clojure deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/ahitrin/agenta"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.logging "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/tools.logging "1.3.0"]
                  [cljfx "1.7.24"]
                  [com.github.javafaker/javafaker "1.0.2"]
                  [ch.qos.logback/logback-classic "1.1.2"]]
